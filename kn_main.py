37641e4fb356ab8559f3b20039b290805cb5acb2
minamibashi rearn <rearn499+git@gmail.com>
2016-01-24 22:05:17 +0900

    Add function kn_read_content
    徐々に関数化

--- kn_main.py
+++ kn_main.py
@@ -3,23 +3,24 @@ import sqlite3
 import markdown
 from markdown.extensions.toc import TocExtension
 
+def kn_read_content(kn_db):
+	conkeylist = ["id", "published", "updated", "title", "author", "context", "status", "next_id", "prev_id"]
+	conn = sqlite3.connect(kn_db)
+	tags = []
 
-conkeylist = ["id", "published", "updated", "title", "author", "context", "status", "next_id", "prev_id"]
-conn = sqlite3.connect('./test/kn.sqlite3')
-tags = []
+	c = conn.cursor()
 
-c = conn.cursor()
+	for row in c.execute('SELECT kn_contents.id, published, updated, title, kn_author.name, context, status, next_id, prev_id FROM kn_contents INNER JOIN kn_author ON kn_contents.author = kn_author.id WHERE kn_contents.id = ? LIMIT 1;', [ 1]):
+		dict_data=dict(zip(conkeylist,list(row)))
 
-for row in c.execute('SELECT kn_contents.id, published, updated, title, kn_author.name, context, status, next_id, prev_id FROM kn_contents INNER JOIN kn_author ON kn_contents.author = kn_author.id WHERE kn_contents.id = ? LIMIT 1;', [ 1]):
-	dict_data=dict(zip(conkeylist,list(row)))
+	for row_tag in c.execute('SELECT kn_tags.tag FROM kn_contents_tags INNER JOIN kn_tags ON kn_contents_tags.tag_id = kn_tags.id WHERE content_id = ?;', [ 1]):
+		tags.append(row_tag[0])
 
-for row_tag in c.execute('SELECT kn_tags.tag FROM kn_contents_tags INNER JOIN kn_tags ON kn_contents_tags.tag_id = kn_tags.id WHERE content_id = ?;', [ 1]):
-	tags.append(row_tag[0])
+	c.close()
+	dict_data["tags"] = tags
+	return json.dumps(dict_data, sort_keys=True, indent=4)
 
-dict_data["tags"] = tags
-json_data=json.dumps(dict_data, sort_keys=True, indent=4)
-print(json_data)
-
+json_data = kn_read_content('./test/kn.sqlite3')
 
 root = json.loads(json_data)
 print('<article itemscope itemType="http://schema.org/BlogPosting">')
