73a0aa3786c6857d2b4bfc77356fb0852a33ee75
minamibashi rearn <rearn499+git@gmail.com>
2016-01-24 17:17:54 +0900

    Remove comment out program code
    デバッグ用のコメントがあったので修正

--- kn_main.py
+++ kn_main.py
@@ -12,14 +12,12 @@ c = conn.cursor()
 
 for row in c.execute('SELECT kn_contents.id, published, updated, title, kn_author.name, context, status, next_id, prev_id FROM kn_contents INNER JOIN kn_author ON kn_contents.author = kn_author.id WHERE kn_contents.id = ? LIMIT 1;', [ 1]):
 	dict_data=dict(zip(conkeylist,list(row)))
-	#json_data=json.dumps(dict_data,  sort_keys=True, indent=4)
-	#print(json_data)
 
 for row_tag in c.execute('SELECT kn_tags.tag FROM kn_contents_tags INNER JOIN kn_tags ON kn_contents_tags.tag_id = kn_tags.id WHERE content_id = ?;', [ 1]):
 	tags.append(row_tag[0])
 
 dict_data["tags"] = tags
-json_data=json.dumps(dict_data,  sort_keys=True, indent=4)
+json_data=json.dumps(dict_data, sort_keys=True, indent=4)
 print(json_data)
 
 
