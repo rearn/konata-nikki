aeb1d04829e60d653affed11fda4087ee72aed7b
minamibashi rearn <rearn499+git@gmail.com>
2016-01-25 22:02:38 +0900

    Fix for SQL SELECT
    SQLが長いので修正

--- kn_main.py
+++ kn_main.py
@@ -3,18 +3,24 @@ import sqlite3
 import markdown
 from markdown.extensions.toc import TocExtension
 
+def dict_factory(cursor, row):
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+	return d
+
 def kn_read_content(kn_db):
-	conkeylist = ["id", "published", "updated", "title", "author", "context", "status", "next_id", "prev_id"]
 	conn = sqlite3.connect(kn_db)
 	tags = []
 
+	conn.row_factory= dict_factory
 	c = conn.cursor()
 
-	for row in c.execute('SELECT kn_contents.id, published, updated, title, kn_author.name, context, status, next_id, prev_id FROM kn_contents INNER JOIN kn_author ON kn_contents.author = kn_author.id WHERE kn_contents.id = ? LIMIT 1;', [ 1]):
-		dict_data=dict(zip(conkeylist,list(row)))
+	for row in c.execute('SELECT * FROM kn_contents INNER JOIN kn_author ON kn_contents.author_id = kn_author.id WHERE kn_contents.id = ? LIMIT 1;', [ 1]):
+		dict_data=row
 
-	for row_tag in c.execute('SELECT kn_tags.tag FROM kn_contents_tags INNER JOIN kn_tags ON kn_contents_tags.tag_id = kn_tags.id WHERE content_id = ?;', [ 1]):
-		tags.append(row_tag[0])
+	for row_tag in c.execute('SELECT * FROM kn_contents_tags INNER JOIN kn_tags ON kn_contents_tags.tag_id = kn_tags.id WHERE content_id = ?;', [ 1]):
+		tags.append(row_tag['tag'])
 
 	c.close()
 	dict_data["tags"] = tags
