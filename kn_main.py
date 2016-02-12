945f30cd33b475a7f7776627d14928a1d0af42ef
minamibashi rearn <rearn499+git@gmail.com>
2016-02-13 04:38:20 +0900

    Add SQL with respect to kn_sites
    サイト名表示の準備

--- kn_main.py
+++ kn_main.py
@@ -13,25 +13,50 @@ def dict_factory(cursor, row):
 def kn_read_content(kn_db):
 	conn = sqlite3.connect(kn_db)
 	tags = []
+	dict_data = {}
 
 	conn.row_factory= dict_factory
 	c = conn.cursor()
 
 	sql_stmt = '''
 		SELECT
-			*
+			kn_sites.id,
+			begun,
+			updated,
+			title,
+			abstract,
+			latest_content_id
+		FROM kn_sites
+		INNER JOIN kn_author
+			ON kn_sites.main_author_id = kn_author.id
+		WHERE kn_sites.id = ?
+		LIMIT 1
+	'''
+	for row in c.execute(sql_stmt, [ 1]): # site 1
+		dict_data['site']=row
+
+	sql_stmt = '''
+		SELECT
+			kn_contents.id,
+			published,
+			updated,
+			title,
+			kn_author.author,
+			context,
+			status,
+			next_id,
+			prev_id
 		FROM kn_contents
 		INNER JOIN kn_author
 			ON kn_contents.author_id = kn_author.id
 		WHERE kn_contents.id = ?
 		LIMIT 1
 	'''
-	for row in c.execute(sql_stmt, [ 1]):
-		dict_data=row
+	dict_data['contents'] = list(c.execute(sql_stmt, [ 1]))
 
 	sql_stmt = '''
 		SELECT
-			*
+			kn_tags.tag
 		FROM kn_contents_tags
 		INNER JOIN kn_tags
 			ON kn_contents_tags.tag_id = kn_tags.id
@@ -41,17 +66,18 @@ def kn_read_content(kn_db):
 		tags.append(row_tag['tag'])
 
 	c.close()
-	dict_data["tags"] = tags
+	dict_data['contents'][0]['tags'] = tags
 	return(json.dumps(dict_data, sort_keys=True, indent=4))
 
 def kn_print_content(json_data):
 	root = json.loads(json_data)
-	con = markdown.markdown(root['context'], extensions=[TocExtension(baselevel=2)], output_format='xhtml5')
+	con = markdown.markdown(root['contents'][0]['context'], extensions=[TocExtension(baselevel=2)], output_format='xhtml5')
 	env = jinja2.Environment(loader=jinja2.FileSystemLoader('./tests/', encoding='utf8'))
 	htmltmpl = env.get_template('contents.material.html.ja')
-	ret = htmltmpl.render({'root':root, 'markdown':con})
+	ret = htmltmpl.render({'root':root['contents'][0], 'markdown':con})
 	return ret
 
 if __name__ == '__main__':
 	json_data = kn_read_content('./tests/kn.sqlite3')
+	print(json_data) # debug
 	print(kn_print_content(json_data))
