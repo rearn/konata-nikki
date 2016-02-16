4c1c09fd8369b0e5d0dd5248710570e15ae284ab
minamibashi rearn <rearn499+git@gmail.com>
2016-02-16 12:27:28 +0900

    Moved by the template file
    template fileをmaterial directoryに移動

--- kn_main.py
+++ kn_main.py
@@ -101,20 +101,20 @@ def kn_read_content(kn_db):
 def kn_print_content(json_data):
 	root = json.loads(json_data)
 	con = markdown.markdown(root['contents'][0]['context'], extensions=[TocExtension(baselevel=2)], output_format='xhtml5')
-	env = jinja2.Environment(loader=jinja2.FileSystemLoader('./tests/', encoding='utf8'))
+	env = jinja2.Environment(loader=jinja2.FileSystemLoader('./material/', encoding='utf8'))
 
-	htmltmpl = env.get_template('nav.material.html.ja')
+	htmltmpl = env.get_template('nav.html.ja')
 	test = {'next': {'uri':'aaa', 'title':'ee'}}
 	#ret_nav = htmltmpl.render({'nav':root['contents'][0]['nav']})
 	ret_nav = htmltmpl.render({'nav':test})
 
-	htmltmpl = env.get_template('header.material.html.ja')
+	htmltmpl = env.get_template('header.html.ja')
 	ret_header = htmltmpl.render({'contents':root['contents'][0], 'site':root['site'], 'nav':ret_nav})
 
-	htmltmpl = env.get_template('footer.material.html.ja')
+	htmltmpl = env.get_template('footer.html.ja')
 	ret_footer = htmltmpl.render({'contents':root['contents'][0], 'site':root['site'], 'nav':ret_nav})
 
-	htmltmpl = env.get_template('contents.material.html.ja')
+	htmltmpl = env.get_template('contents.html.ja')
 	ret_contents = htmltmpl.render({'root':root['contents'][0], 'markdown':con})
 
 	return '\n'.join([ret_header, ret_contents, ret_footer])
